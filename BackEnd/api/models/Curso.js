8f583aebfeb03f5bd99c00a77b707f40a27915a4
AndresSal <andres.salazar01@epn.edu.ec>
2018-01-21 18:36:00 -0500

    Creación de la entidad curso

---
+++ BackEnd/api/models/Curso.js
@@ -0,0 +1,32 @@
+/**
+ * Curso.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    nombre:
+      {
+        type:'String',
+        defaultsTo: 'NULL'
+      },
+    descripcion:
+      {
+        type:'String',
+        defaultsTo:'NULL'
+      },
+    num_horas:
+      {
+        type:'int'
+      },
+    coste_curso:
+      {
+        type:'float'
+      }
+  },
+  connection:'mongodb'
+};
+
