6e5205f89c3c8ced7f2d55dfb20daf454a598a2a
AndresSal <andres.salazar01@epn.edu.ec>
2018-01-21 19:52:01 -0500

    Controlador - agregar un nuevo curso

--- BackEnd/api/controllers/CursoController.js
+++ BackEnd/api/controllers/CursoController.js
@@ -6,6 +6,19 @@
  */
 
 module.exports = {
+
+    agregar_curso:function(req, res)
+    {
+      var aux=req.allParams();
+      Curso.create({nombre:aux.nombre, descripcion:aux.descripcion, num_horas:aux.num_horas, coste_horas:aux.coste_horas}).exec(function(err, cursonuevo){
+        if(err)
+        {
+          res.send(500,{error:"Error de Nase de datos"});
+        }
+        res.json(cursonuevo);
+      })
+    },
+
 	  listar_cursos:function(req, res)
     {
       Curso.find({}).exec(function(err, cursos){
