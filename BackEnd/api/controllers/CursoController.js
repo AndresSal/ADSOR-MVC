fe0e651ce642f6397878deb20918da4f9dd76001
AndresSal <andres.salazar01@epn.edu.ec>
2018-01-21 19:27:33 -0500

    Controlador - desplegar lista de cursos

--- BackEnd/api/controllers/CursoController.js
+++ BackEnd/api/controllers/CursoController.js
@@ -6,6 +6,15 @@
  */
 
 module.exports = {
-	
+	  listar_cursos:function(req, res)
+    {
+      Curso.find({}).exec(function(err, cursos){
+        if(err)
+        {
+          res.send(500,{error:"error de base de datos"});
+        }
+        res.json(cursos);
+      })
+    }
 };
 
